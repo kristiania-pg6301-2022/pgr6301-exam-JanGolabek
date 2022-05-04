01a7846fceefba751c51217700650ae24915b658
JanGolabek <77743690+JanGolabek@users.noreply.github.com>
2022-05-04 23:58:58 +0200

    Use React-Router

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("app"));
+function FrontPage() {
+    return (
+        <div>
+            <h1>Article Database</h1>
+            <ul>
+                <li>
+                    <Link to={"/articles"}>List articles</Link>
+                </li>
+                <li>
+                    <Link to={"/articles/new"}>Add new article</Link>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+function ListArticles() {
+    return (
+        <div>
+            <h1>Articles in the database</h1>
+        </div>
+    );
+}
+
+function AddNewArticle() {
+    return (
+        <form>
+            <h1>Add new Article</h1>
+        </form>
+    );
+}
+
+function Application() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<FrontPage />} />
+                <Route path={"/articles"} element={<ListArticles />} />
+                <Route path={"/articles/new"} element={<AddNewArticle />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
