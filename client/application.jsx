0ea05251dd47f56d73c043cd3d3748b1231cb2d4
JanGolabek <77743690+JanGolabek@users.noreply.github.com>
2022-05-05 23:26:00 +0200

    Added layout in css

--- client/application.jsx
+++ client/application.jsx
@@ -1,20 +1,29 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { FrontPage } from "./pages/frontPage";
 import { ListArticles } from "./pages/listArticles";
 import { AddNewArticle } from "./pages/addNewArticle";
 import React from "react";
+import "./application.css";
+
 
 export function Application() {
 
 
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path={"/"} element={<FrontPage />} />
-                <Route path={"/articles"} element={<ListArticles />} />
-                />
-                <Route path={"/articles/new"} element={<AddNewArticle />} />
-            </Routes>
+            <header>
+                <Link to={"/"}>Front page</Link>
+                <Link to={"/articles"}>Articles</Link>
+                <div className="menu-divider" />
+                <Link to={"/login"}>Login</Link>
+            </header>
+            <main>
+                <Routes>
+                    <Route path={"/"} element={<FrontPage />} />
+                    <Route path={"/articles"} element={<ListArticles />} />
+                    <Route path={"/articles/new"} element={<AddNewArticle />} />
+                </Routes>
+            </main>
         </BrowserRouter>
     );
 }
