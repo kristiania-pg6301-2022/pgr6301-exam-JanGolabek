3fc4c501417286b02616ad0756cd3304b4630689
JanGolabek <77743690+JanGolabek@users.noreply.github.com>
2022-05-05 14:36:59 +0200

    Test shows list of articles from the db

--- client/__tests__/listArticles.test.jsx
+++ client/__tests__/listArticles.test.jsx
@@ -2,11 +2,21 @@ import { ListArticles } from "../listArticles";
 
 import React from "react";
 import ReactDOM from "react-dom";
+import {act} from "react-dom/test-utils";
 
 describe("ListArticles component", () => {
     it("shows loading screen", () => {
         const domElement = document.createElement("div");
         ReactDOM.render(<ListArticles />, domElement);
+        expect(domElement.innerHTML).toMatchSnapshot();
+    });
+
+    it("shows articles", async () => {
+        const articles = [{ title: "article 1" }, { title: "article 2" }];
+        const domElement = document.createElement("div");
+        await act(async () => {
+            ReactDOM.render(<ListArticles listArticles={() => articles} />, domElement);
+        });
         expect(domElement.innerHTML).toMatchSnapshot();
     });
 });
