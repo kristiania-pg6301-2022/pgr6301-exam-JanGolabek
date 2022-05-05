ea50c9886a691e77e22663549e69b9c325b441a6
JanGolabek <77743690+JanGolabek@users.noreply.github.com>
2022-05-05 14:23:14 +0200

    jest testing

--- client/__tests__/listArticles.test.jsx
+++ client/__tests__/listArticles.test.jsx
@@ -1,3 +1,12 @@
+import { ListArticles } from "../listArticles";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
 describe("ListArticles component", () => {
-    it("shows loading screen", () => {});
+    it("shows loading screen", () => {
+        const domElement = document.createElement("div");
+        ReactDOM.render(<ListArticles />, domElement);
+        expect(domElement.innerHTML).toMatchSnapshot();
+    });
 });
